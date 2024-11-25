faea5fe716d3aca80be859cbc2201ef53c25a2f5
Roldanuriel <uriroldan06@gmail.com>
2024-11-25 13:45:37 -0600

    cambie el c olor del panel de inicio

--- SistemaVentas/Inicio.Designer.cs
+++ SistemaVentas/Inicio.Designer.cs
@@ -191,6 +191,7 @@
             // 
             // Contenedor
             // 
+            this.Contenedor.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Contenedor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Contenedor.Location = new System.Drawing.Point(0, 130);
             this.Contenedor.Name = "Contenedor";
